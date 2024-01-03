0fb6b742f849f7a550148030cdaf03e0dacb4ce2
Armin Herling <armin.herling@gmail.com>
2024-01-03 22:07:17 +0100

    Some cleanup in MultipleSourceLocations test

--- Tests/SourceLocationTests/source/SourceLocationTests.cpp
+++ Tests/SourceLocationTests/source/SourceLocationTests.cpp
@@ -74,24 +74,25 @@ namespace
     void MultipleSourceLocations()
     {
         auto input = std::make_shared<SourceText>(QString("define sum(a int, b int) \r\n {\r\n return a + b \r\n}\r\n"));
-        auto expectedList = QList<SourceLocation>()
-            << SourceLocation{.source = input, .startIndex = 0, .endIndex = 6, .startColumn = 1, .endColumn = 7, .startLine = 1, .endLine = 1 }  // define
-            << SourceLocation{.source = input, .startIndex = 7, .endIndex = 10, .startColumn = 8, .endColumn = 11, .startLine = 1, .endLine = 1} // sum
-            << SourceLocation{.source = input, .startIndex = 10, .endIndex = 11, .startColumn = 11, .endColumn = 12, .startLine = 1, .endLine = 1} // (
-            << SourceLocation{.source = input, .startIndex = 11, .endIndex = 12, .startColumn = 12, .endColumn = 13, .startLine = 1, .endLine = 1} // a
-            << SourceLocation{.source = input, .startIndex = 13, .endIndex = 16, .startColumn = 14, .endColumn = 17, .startLine = 1, .endLine = 1} // int
-            << SourceLocation{.source = input, .startIndex = 16, .endIndex = 17, .startColumn = 17, .endColumn = 18, .startLine = 1, .endLine = 1} // ,
-            << SourceLocation{.source = input, .startIndex = 18, .endIndex = 19, .startColumn = 19, .endColumn = 20, .startLine = 1, .endLine = 1} // b
-            << SourceLocation{.source = input, .startIndex = 20, .endIndex = 23, .startColumn = 21, .endColumn = 24, .startLine = 1, .endLine = 1} // int
-            << SourceLocation{.source = input, .startIndex = 23, .endIndex = 24, .startColumn = 24, .endColumn = 25, .startLine = 1, .endLine = 1} // )
-            << SourceLocation{.source = input, .startIndex = 28, .endIndex = 29, .startColumn = 2, .endColumn = 3, .startLine = 2, .endLine = 2}  // {
-            << SourceLocation{.source = input, .startIndex = 32, .endIndex = 38, .startColumn = 2, .endColumn = 8, .startLine = 3, .endLine = 3}  // return
-            << SourceLocation{.source = input, .startIndex = 39, .endIndex = 40, .startColumn = 9, .endColumn = 10, .startLine = 3, .endLine = 3}  // a
-            << SourceLocation{.source = input, .startIndex = 41, .endIndex = 42, .startColumn = 11, .endColumn = 12, .startLine = 3, .endLine = 3} // +
-            << SourceLocation{.source = input, .startIndex = 43, .endIndex = 44, .startColumn = 13, .endColumn = 14, .startLine = 3, .endLine = 3} // b
-            << SourceLocation{.source = input, .startIndex = 47, .endIndex = 48, .startColumn = 1, .endColumn = 2, .startLine = 4, .endLine = 4}  // }
-            << SourceLocation{.source = input, .startIndex = 50, .endIndex = 50, .startColumn = 1, .endColumn = 1, .startLine = 5, .endLine = 5}; // EOL
-
+        auto expectedList = QList<SourceLocation>
+        {
+            { .source = input, .startIndex = 0, .endIndex = 6, .startColumn = 1, .endColumn = 7, .startLine = 1, .endLine = 1 },  // define
+            { .source = input, .startIndex = 7, .endIndex = 10, .startColumn = 8, .endColumn = 11, .startLine = 1, .endLine = 1 }, // sum
+            { .source = input, .startIndex = 10, .endIndex = 11, .startColumn = 11, .endColumn = 12, .startLine = 1, .endLine = 1 }, // (
+            { .source = input, .startIndex = 11, .endIndex = 12, .startColumn = 12, .endColumn = 13, .startLine = 1, .endLine = 1 }, // a
+            { .source = input, .startIndex = 13, .endIndex = 16, .startColumn = 14, .endColumn = 17, .startLine = 1, .endLine = 1 }, // int
+            { .source = input, .startIndex = 16, .endIndex = 17, .startColumn = 17, .endColumn = 18, .startLine = 1, .endLine = 1 }, // ,
+            { .source = input, .startIndex = 18, .endIndex = 19, .startColumn = 19, .endColumn = 20, .startLine = 1, .endLine = 1 }, // b
+            { .source = input, .startIndex = 20, .endIndex = 23, .startColumn = 21, .endColumn = 24, .startLine = 1, .endLine = 1 }, // int
+            { .source = input, .startIndex = 23, .endIndex = 24, .startColumn = 24, .endColumn = 25, .startLine = 1, .endLine = 1 }, // )
+            { .source = input, .startIndex = 28, .endIndex = 29, .startColumn = 2, .endColumn = 3, .startLine = 2, .endLine = 2 },  // {
+            { .source = input, .startIndex = 32, .endIndex = 38, .startColumn = 2, .endColumn = 8, .startLine = 3, .endLine = 3 },  // return
+            { .source = input, .startIndex = 39, .endIndex = 40, .startColumn = 9, .endColumn = 10, .startLine = 3, .endLine = 3 },  // a
+            { .source = input, .startIndex = 41, .endIndex = 42, .startColumn = 11, .endColumn = 12, .startLine = 3, .endLine = 3 }, // +
+            { .source = input, .startIndex = 43, .endIndex = 44, .startColumn = 13, .endColumn = 14, .startLine = 3, .endLine = 3 }, // b
+            { .source = input, .startIndex = 47, .endIndex = 48, .startColumn = 1, .endColumn = 2, .startLine = 4, .endLine = 4 },  // }
+            { .source = input, .startIndex = 50, .endIndex = 50, .startColumn = 1, .endColumn = 1, .startLine = 5, .endLine = 5 }, // EOL
+        };
         DiagnosticsBag diagnostics;
 
         auto startTime = std::chrono::high_resolution_clock::now();
